31fa617072a34b71d7913dae530cd79f75a78b3c
Mark Harmstone <mark@harmstone.com>
2023-02-20 23:02:39 +0000

    efi_main: register driver binding

--- src/ntfs.c
+++ src/ntfs.c
@@ -1,7 +1,57 @@
 #include <efi.h>
 
-EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
+static EFI_SYSTEM_TABLE* systable;
+static EFI_BOOT_SERVICES* bs;
+static EFI_DRIVER_BINDING_PROTOCOL drvbind;
+
+static void do_print_error(const char* func, EFI_STATUS Status) {
     // FIXME
+}
+
+static EFI_STATUS drv_supported(EFI_DRIVER_BINDING_PROTOCOL* This, EFI_HANDLE ControllerHandle,
+                                EFI_DEVICE_PATH_PROTOCOL* RemainingDevicePath) {
+    // FIXME
+    systable->ConOut->OutputString(systable->ConOut, L"drv_supported\r\n");
+
+    return EFI_INVALID_PARAMETER;
+}
+
+static EFI_STATUS EFIAPI drv_start(EFI_DRIVER_BINDING_PROTOCOL* This, EFI_HANDLE ControllerHandle,
+                                   EFI_DEVICE_PATH_PROTOCOL* RemainingDevicePath) {
+    systable->ConOut->OutputString(systable->ConOut, L"drv_start\r\n");
+    // FIXME
+
+    return EFI_INVALID_PARAMETER;
+}
+
+static EFI_STATUS EFIAPI drv_stop(EFI_DRIVER_BINDING_PROTOCOL* This, EFI_HANDLE ControllerHandle,
+                                  UINTN NumberOfChildren, EFI_HANDLE* ChildHandleBuffer) {
+    systable->ConOut->OutputString(systable->ConOut, L"drv_stop\r\n");
+    // FIXME
+
+    return EFI_INVALID_PARAMETER;
+}
+
+EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
+    EFI_STATUS Status;
+    EFI_GUID guid = EFI_DRIVER_BINDING_PROTOCOL_GUID;
+
+    systable = SystemTable;
+    bs = SystemTable->BootServices;
+
+    drvbind.Supported = drv_supported;
+    drvbind.Start = drv_start;
+    drvbind.Stop = drv_stop;
+    drvbind.Version = 0x10;
+    drvbind.ImageHandle = ImageHandle;
+    drvbind.DriverBindingHandle = ImageHandle;
+
+    Status = bs->InstallProtocolInterface(&drvbind.DriverBindingHandle, &guid,
+                                          EFI_NATIVE_INTERFACE, &drvbind);
+    if (EFI_ERROR(Status)) {
+        do_print_error("InstallProtocolInterface", Status);
+        return Status;
+    }
 
     return EFI_SUCCESS;
 }
