884b3130c67ab05dd08d3751a371f6a4fcf74825
Mark Harmstone <mark@harmstone.com>
2023-02-20 23:05:34 +0000

    drv_supported: only pick up block devices

--- src/ntfs.c
+++ src/ntfs.c
@@ -1,5 +1,7 @@
 #include <efi.h>
 
+#define UNUSED(x) (void)(x)
+
 static EFI_SYSTEM_TABLE* systable;
 static EFI_BOOT_SERVICES* bs;
 static EFI_DRIVER_BINDING_PROTOCOL drvbind;
@@ -10,10 +12,23 @@ static void do_print_error(const char* func, EFI_STATUS Status) {
 
 static EFI_STATUS drv_supported(EFI_DRIVER_BINDING_PROTOCOL* This, EFI_HANDLE ControllerHandle,
                                 EFI_DEVICE_PATH_PROTOCOL* RemainingDevicePath) {
-    // FIXME
-    systable->ConOut->OutputString(systable->ConOut, L"drv_supported\r\n");
+    EFI_STATUS Status;
+    EFI_DISK_IO_PROTOCOL* disk_io;
+    EFI_GUID guid_disk = EFI_DISK_IO_PROTOCOL_GUID;
+    EFI_GUID guid_block = EFI_BLOCK_IO_PROTOCOL_GUID;
 
-    return EFI_INVALID_PARAMETER;
+    UNUSED(RemainingDevicePath);
+
+    Status = bs->OpenProtocol(ControllerHandle, &guid_disk, (void**)&disk_io, This->DriverBindingHandle,
+                              ControllerHandle, EFI_OPEN_PROTOCOL_BY_DRIVER);
+
+    if (EFI_ERROR(Status))
+        return Status;
+
+    bs->CloseProtocol(ControllerHandle, &guid_disk, This->DriverBindingHandle, ControllerHandle);
+
+    return bs->OpenProtocol(ControllerHandle, &guid_block, NULL, This->DriverBindingHandle,
+                            ControllerHandle, EFI_OPEN_PROTOCOL_TEST_PROTOCOL);
 }
 
 static EFI_STATUS EFIAPI drv_start(EFI_DRIVER_BINDING_PROTOCOL* This, EFI_HANDLE ControllerHandle,
