d15f856bc787ec02201540089af1442d5c85387d
Mark Harmstone <mark@harmstone.com>
2023-03-20 02:47:40 +0000

    add copyright

--- src/ntfs.h
+++ src/ntfs.h
@@ -1,3 +1,20 @@
+/* Copyright (c) Mark Harmstone 2023
+ *
+ * This file is part of ntfs-efi.
+ *
+ * ntfs-efi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public Licence as published by
+ * the Free Software Foundation, either version 3 of the Licence, or
+ * (at your option) any later version.
+ *
+ * ntfs-efi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public Licence for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public Licence
+ * along with ntfs-efi.  If not, see <http://www.gnu.org/licenses/>. */
+
 #pragma once
 
 enum class ntfs_attribute : uint32_t {
